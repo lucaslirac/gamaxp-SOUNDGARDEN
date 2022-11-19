009aaf957a3940e51f3e06215175c24e64f5c834
Lucas <lucas_lirac@outlook.com>
2022-11-18 22:40:50 -0300

    adicionando modal

--- js/eventos.js
+++ js/eventos.js
@@ -1,28 +1,64 @@
-const SOUND_URL = 'https://xp41-soundgarden-api.herokuapp.com/events';
-const listarEventos = async () => {
-    const eventos = await fetch(SOUND_URL, {
-        method: "GET",
-        mode: "cors",
+// EVENTOS
+const eventosContainer = document.querySelector('#eventocontainer');
+const url = "https://xp41-soundgarden-api.herokuapp.com";
+
+async function listaEventos() {
+    const response = await fetch(
+      `https://xp41-soundgarden-api.herokuapp.com/events`,
+      {
         headers: {
-            "Content-Type": "application/json"
-        }
-    }).then((resposta) => {
-        
-        return resposta.json();
+          'Content-type': 'application/json',
+        },
+      }
+    );
+  
+    const eventos = await response.json();
+
+    eventos.forEach( evento => {
+        const cardEventos = `
+        <article class="evento card p-5 m-3">
+        <h2>${evento.name}</h2>
+        <h4>${evento.attractions}</h4>
+        <p>${evento.description}</p>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addReservaModal">
+        reservar ingresso
+      </button>
+        </article>`
+   
+        eventosContainer.innerHTML += cardEventos;
     });
-    
-    const tbody = document.querySelector('.exibe-event');
-    let htmlEventos = "";
-    eventos.forEach(evento => {
-        htmlEventos +=`
-                    <article class="evento card p-5 m-3">
-                    <h2>${evento.name} - ${evento.scheduled}</h2>
-                    <h4>${evento.attractions.join(', ')}</h4>
-                    <p>${evento.description}</p>
-                    <a href="reservar-ingresso.html?id=${evento._id}" class="btn btn-primary">reservar ingresso</a>
-                    </article>
-                    `;
-            });
-    tbody.innerHTML = htmlEventos;
-}
-listarEventos();
+  }
+  
+listaEventos();
+
+//MODAL
+const modalReserva = document.getElementById("addReservaModal");
+const formReserva = modalReserva.querySelector("form");
+
+const params = new URLSearchParams(location.search)
+  params.get('id');
+
+formReserva.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const dados = {
+    owner_name: document.getElementById('nome').value,
+    owner_email: document.getElementById('email').value,
+    number_tickets: 1,
+    event_id: ""
+    }; 
+
+  fetch(`${url}/bookings/`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dados),
+  })
+    .then(() => {
+      alert("Reserva feita com sucesso");
+      window.location.replace("eventos.html")
+    })
+    .catch((error) => console.log(error.message));
+});
